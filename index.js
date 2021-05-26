089164d42a10c748930e95326d7353feec3e2371
oniant <emplangle@gmail.com>
2021-05-26 23:04:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/type', function(request, writer) {
     });
 });
 
-var server = app.listen(20001, function() {
+var server = app.listen(80, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log("service up at http://%s:%s", host, port);
